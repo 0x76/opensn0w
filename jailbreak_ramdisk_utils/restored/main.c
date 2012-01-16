facf04c4c2a46b3720afc36400056e9bdf527e2b
rms <winocm@irc6.tsu-ji.jp>
2012-01-16 15:25:57 -0600

    Usbmux support, fires up /sbin/sshd.

--- jailbreak_ramdisk_utils/restored/main.c
+++ jailbreak_ramdisk_utils/restored/main.c
@@ -17,19 +17,204 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
 
-#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/sockio.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
+#include <net/if.h>
+#include <assert.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <AvailabilityMacros.h>
+#define MAC_OS_X_VERSION_MIN_REQUIRED MAC_OS_X_VERSION_10_5
+#include <IOKit/IOCFPlugIn.h>
+
+#include "IOUSBDeviceControllerLib.h"
+
 #include "hfs_mount.h"
+
+char *execve_env[] = { NULL };
+char *execve_params[] = { "/sbin/sshd", NULL };
+
+/* start comex code */
+#define kIOSomethingPluginID CFUUIDGetConstantUUIDWithBytes(NULL, \
+0x9E, 0x72, 0x21, 0x7E, 0x8A, 0x60, 0x11, 0xDB, \
+0xBF, 0x57, 0x00, 0x0D, 0x93, 0x6D, 0x06, 0xD2)
+#define kIOWhatTheFuckID CFUUIDGetConstantUUIDWithBytes(NULL, \
+0xEA, 0x33, 0xBA, 0x4F, 0x8A, 0x60, 0x11, 0xDB, \
+0x84, 0xDB, 0x00, 0x0D, 0x93, 0x6D, 0x06, 0xD2)
+
+void init_usb()
+{
+	IOUSBDeviceDescriptionRef desc =
+	    IOUSBDeviceDescriptionCreateFromDefaults(kCFAllocatorDefault);
+	IOUSBDeviceDescriptionSetSerialString(desc,
+					      CFSTR("ramdisk tool " __DATE__ " "
+						    __TIME__));
+
+	CFArrayRef usb_interfaces = IOUSBDeviceDescriptionCopyInterfaces(desc);
+	int i;
+	for (i = 0; i < CFArrayGetCount(usb_interfaces); i++) {
+		CFArrayRef arr1 = CFArrayGetValueAtIndex(usb_interfaces, i);
+
+		if (CFArrayContainsValue
+		    (arr1, CFRangeMake(0, CFArrayGetCount(arr1)), CFSTR("PTP")))
+		{
+			printf("Found PTP interface\n");
+			break;
+		}
+	}
+
+	IOUSBDeviceControllerRef controller;
+	while (IOUSBDeviceControllerCreate(kCFAllocatorDefault, &controller)) {
+		printf("Unable to get USB device controller\n");
+		sleep(3);
+	}
+	IOUSBDeviceControllerSetDescription(controller, desc);
+
+	CFMutableDictionaryRef match =
+	    IOServiceMatching("IOUSBDeviceInterface");
+	CFMutableDictionaryRef dict =
+	    CFDictionaryCreateMutable(NULL, 0, &kCFTypeDictionaryKeyCallBacks,
+				      &kCFTypeDictionaryValueCallBacks);
+	CFDictionarySetValue(dict, CFSTR("USBDeviceFunction"), CFSTR("PTP"));
+	CFDictionarySetValue(match, CFSTR("IOPropertyMatch"), dict);
+	io_service_t service;
+	while (1) {
+		service =
+		    IOServiceGetMatchingService(kIOMasterPortDefault, match);
+		if (!service) {
+			printf("Didn't find, trying again\n");
+			sleep(1);
+		} else {
+			break;
+		}
+	}
+	IOCFPlugInInterface **iface;
+	SInt32 score;
+	printf("123\n");
+	assert(!IOCreatePlugInInterfaceForService(service,
+						  kIOSomethingPluginID,
+						  kIOCFPlugInInterfaceID,
+						  &iface, &score));
+	void *thing;
+
+	assert(!((*iface)->QueryInterface) (iface,
+					    CFUUIDGetUUIDBytes
+					    (kIOWhatTheFuckID), &thing));
+
+	IOReturn(**table) (void *,...) = *((void **)thing);
+	printf("%p\n", table[0x10 / 4]);
+
+	//open IOUSBDeviceInterfaceInterface
+	(!table[0x10 / 4] (thing, 0));
+	//set IOUSBDeviceInterfaceInterface class
+	(!table[0x2c / 4] (thing, 0xff, 0));
+	//set IOUSBDeviceInterfaceInterface sub-class
+	(!table[0x30 / 4] (thing, 0x50, 0));
+	//set IOUSBDeviceInterfaceInterface protocol
+	(!table[0x34 / 4] (thing, 0x43, 0));
+	//commit IOUSBDeviceInterfaceInterface configuration
+	(!table[0x44 / 4] (thing, 0));
+	IODestroyPlugInInterface(iface);
+	//assert(!table[0x14/4](thing, 0));
+}
+
+void init_tcp()
+{
+	// from launchd
+	struct ifaliasreq ifra;
+	struct ifreq ifr;
+	int s;
+
+	memset(&ifr, 0, sizeof(ifr));
+	strcpy(ifr.ifr_name, "lo0");
+
+	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+		return;
+
+	if (ioctl(s, SIOCGIFFLAGS, &ifr) != -1) {
+		ifr.ifr_flags |= IFF_UP;
+		assert(ioctl(s, SIOCSIFFLAGS, &ifr) != -1);
+	}
+
+	memset(&ifra, 0, sizeof(ifra));
+	strcpy(ifra.ifra_name, "lo0");
+	((struct sockaddr_in *)&ifra.ifra_addr)->sin_family = AF_INET;
+	((struct sockaddr_in *)&ifra.ifra_addr)->sin_addr.s_addr =
+	    htonl(INADDR_LOOPBACK);
+	((struct sockaddr_in *)&ifra.ifra_addr)->sin_len =
+	    sizeof(struct sockaddr_in);
+	((struct sockaddr_in *)&ifra.ifra_mask)->sin_family = AF_INET;
+	((struct sockaddr_in *)&ifra.ifra_mask)->sin_addr.s_addr =
+	    htonl(IN_CLASSA_NET);
+	((struct sockaddr_in *)&ifra.ifra_mask)->sin_len =
+	    sizeof(struct sockaddr_in);
+
+	assert(ioctl(s, SIOCAIFADDR, &ifra) != -1);
+
+	assert(close(s) == 0);
+
+}
+
+/* end comex code */
+
+char *get_system_platform(void)
+{
+	size_t size;
+	char *machine;
+
+	/* get machine length */
+	sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+
+	/* get string */
+	machine = malloc(size);
+	if (!machine) {
+		return NULL;
+	}
+
+	sysctlbyname("hw.machine", machine, &size, NULL, 0);
+
+	return machine;
+}
 
 int main(int argc, char *argv[], char *env[])
 {
 	struct stat status;
 	int ret = 0, i;
+	char *platform;
 
 	for (i = 0; i < 100; i++) {
 		printf("\n");
 	}
+
+	/* from iPhone-dataprotection restored */
+	CFMutableDictionaryRef matching;
+	io_service_t service = 0;
+	matching = IOServiceMatching("IOWatchDogTimer");
+	if (matching == NULL) {
+		printf
+		    ("unable to create matching dictionary for class IOWatchDogTimer\n");
+	}
+
+	service = IOServiceGetMatchingService(kIOMasterPortDefault, matching);
+	if (service == 0) {
+		printf
+		    ("unable to create matching dictionary for class IOWatchDogTimer\n");
+	}
+	uint32_t zero = 0;
+	CFNumberRef n =
+	    CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &zero);
+	IORegistryEntrySetCFProperties(service, n);
+	IOObjectRelease(service);
+
+	init_tcp();
+	init_usb();
+	printf("USB initialization complete.\n");
+	/* end */
 
 	printf("Waiting for disk to mount...\n");
 
@@ -42,7 +227,7 @@ int main(int argc, char *argv[], char *env[])
 	printf("disk0s1 is alive. Houston, we hear you loud and clear.\n");
 
 	/* at this point, check the disk */
-	printf("Waiting for partitions...\n");
+	printf("Waiting for data partition...\n");
 	for (i = 0; i < 10; i++) {
 		if (!stat("/dev/disk0s2s1", &status)) {
 			system("/sbin/fsck_hfs -fy /dev/disk0s2s1");
@@ -54,14 +239,21 @@ int main(int argc, char *argv[], char *env[])
 			system("/sbin/mount_hfs /dev/disk0s1s2 /mnt2");
 			break;
 		}
-		if (!stat("/dev/disk0s1", &status)) {
-			system("/sbin/fsck_hfs -fy /dev/disk0s1");
-			system("/sbin/mount_hfs /dev/disk0s1 /mnt1");
-			break;
-		}
+		sleep(5);
+	}
+
+	sleep(2);
+
+	printf("Waiting for rootfs partition...\n");
+	for (i = 0; i < 10; i++) {
 		if (!stat("/dev/disk0s1s1", &status)) {
 			system("/sbin/fsck_hfs -fy /dev/disk0s1s1");
 			system("/sbin/mount_hfs /dev/disk0s1s1 /mnt1");
+			break;
+		}
+		if (!stat("/dev/disk0s1", &status)) {
+			system("/sbin/fsck_hfs -fy /dev/disk0s1");
+			system("/sbin/mount_hfs /dev/disk0s1 /mnt1");
 			break;
 		}
 		sleep(5);
@@ -71,9 +263,39 @@ int main(int argc, char *argv[], char *env[])
 	printf("Filesystem mounted to /mnt1.\n");
 
 	printf("Making RAM disk rw...\n");
-	system("mount /");
+	system("mount -uw /");
 
-	/* mount user partition */
+	/* get name */
+	platform = get_system_platform();
+
+	printf("We are running on a %s! ... Checking status: ", platform);
+
+	/* check against supported devices */
+
+	if (!strcmp(platform, "iPod4,1")) {
+		printf("supported.\n");
+	} else if (!strcmp(platform, "iPod3,1")) {
+		printf("supported.\n");
+	} else if (!strcmp(platform, "iPhone2,1")) {
+		printf("supported.\n");
+	} else if (!strcmp(platform, "iPhone3,1")) {
+		printf("supported.\n");
+	} else if (!strcmp(platform, "iPhone3,3")) {
+		printf("supported.\n");
+	} else if (!strcmp(platform, "iPad1,1")) {
+		printf("supported.\n");
+	} else {
+		printf("unsupported.\n");
+		return -1;
+	}
+
+	/* set permissions on ssh keys */
+	chmod("/var/root/.ssh/authorized_keys", 0600);
+	chown("/var/root/.ssh/authorized_keys", 0, 0);
+	chown("/var/root/.ssh", 0, 0);
+	chown("/var/root/", 0, 0);
+
+	/* i need to make a usbmuxd daemon HERE */
 
 	printf(" #######  ##    ##\n");
 	printf("##     ## ##   ## \n");
@@ -83,7 +305,16 @@ int main(int argc, char *argv[], char *env[])
 	printf("##     ## ##   ## \n");
 	printf(" #######  ##    ##\n");
 
-	/* BUT FOR NOW JUST SPIN */
+	printf
+	    ("NOW WITNESS THE FIREPOWER OF THIS FULLY ARMED AND OPERATIONAL BATTLE STATION!\n");
+
+	if (!fork()) {
+		printf("Running %s...\n", execve_params[0]);
+		execve(execve_params[0], execve_params, execve_env);
+		return 0;
+	}
+
+	/* BUT FOR NOW JUST SPIN.. or not? */
 	while (1) ;
 
 	return 0;
